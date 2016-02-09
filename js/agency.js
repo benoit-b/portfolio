1c09a90c6381ef9656641f0a8320f17b66cf09aa
benoit-b <benoit_41@live.fr>
2016-02-09 22:56:59 +0100

    portfolio version 2016 final
    Modification du design, ajout d'éléments

---
+++ js/agency.js
@@ -0,0 +1,53 @@
+/*!
+ * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+
+//progress bar 
+
+$(function(){
+  var $ppc = $('.progress-pie-chart'),
+    percent = parseInt($ppc.data('percent')),
+    deg = 360*percent/100;
+  if (percent > 50) {
+    $ppc.addClass('gt-50');
+  }
+  $('.ppc-progress-fill').css('transform','rotate('+ deg +'deg)');
+  $('.ppc-percents span').html(percent+'%');
+});
+ 
+(function($){
+
+        /* ---------------------------------------------- /*
+         * Preloader
+        /* ---------------------------------------------- */
+
+        $(window).load(function() {
+          $('#status').fadeOut();
+          $('#preloader').delay(300).fadeOut('slow');
+        });
+
+})(jQuery);
